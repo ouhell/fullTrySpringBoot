c375dfefe9a82601c975b8cde51d9e1ce1573314
oussama hamida <oussamabarca200@gmail.com>
2022-07-16 14:43:24 +0100

    first commit

---
+++ src/main/java/com/codebakers/com/fullTrySpringBoot/FullTrySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codebakers.com.fullTrySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullTrySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullTrySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebakers/com/fullTrySpringBoot/FullTrySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebakers.com.fullTrySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullTrySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
